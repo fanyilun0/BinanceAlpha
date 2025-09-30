9c26a1d706518ae66100e3de4c9a0dc811d9f8a0
fanyilun0 <fanyilun0@gmail.com>
2025-09-30 15:26:49 +0800

    更新DeepSeek （DeepSeek-V3.2-Exp）配置文件

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ DEEPSEEK_AI = {
     'model': os.getenv('DEEPSEEK_MODEL', 'deepseek-reasoner'),
     'api_key': os.getenv('DEEPSEEK_API_KEY', ''),
     'temperature': 0,
-    'max_tokens': 64000,
+    'max_tokens': 128000,
     'top_p': 1.0,
     'stream': False,
     'timeout': int(os.getenv('DEEPSEEK_API_TIMEOUT', '900'))  # API请求超时时间(秒)
